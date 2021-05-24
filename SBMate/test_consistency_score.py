f0760a5bd9bbffdff50829e5159104d3efa0c71f
Woosub Shin <woosubs@umich.edu>
2021-05-24 16:23:07 +0900

    updated consistency score testing

--- SBMate/test_consistency_score.py
+++ SBMate/test_consistency_score.py
@@ -49,7 +49,7 @@ class TestConsistencyScore(unittest.TestCase):
     self.assertTrue(cs.isSBOConsistent('SBO:0000284', libsbml.Compartment))
     self.assertTrue(cs.isSBOConsistent(['SBO:0000241'], libsbml.Species))
 
-  def testISCHEBIConsistent(self):
+  def testIsCHEBIConsistent(self):
     # if the term is not in CHEBI tree, for example a GO term is given
     self.assertFalse(cs.isCHEBIConsistent('GO:0035556', libsbml.Reaction))
     # Reaction should not have any CHEBI term
@@ -57,17 +57,27 @@ class TestConsistencyScore(unittest.TestCase):
     # Species can have a correct CHEBI term
     self.assertTrue(cs.isCHEBIConsistent('CHEBI:28087', libsbml.Species))
 
-  def testisUNIPROTConsistent(self):
+  def testIsUNIPROTConsistent(self):
     self.assertFalse(cs.isUNIPROTConsistent('GO:0035556', libsbml.Reaction))
     self.assertFalse(cs.isUNIPROTConsistent('P0DP23', libsbml.Compartment))
     self.assertTrue(cs.isUNIPROTConsistent(['P0DP23'], libsbml.Species))
     self.assertFalse(cs.isUNIPROTConsistent(['P0DP23', 'C94967'], libsbml.Species))
 
+  def testIsKEGGSpeciesConsistent(self):
+    # normally expected case
+    self.assertTrue(cs.isKEGGSpeciesConsistent(['C00062'], libsbml.Species))
+    # compartment should not be a KEGG species term
+    self.assertFalse(cs.isKEGGSpeciesConsistent(['C00062'], libsbml.Compartment))
+    # GO term is not a KEGG term, should be rejected
+    self.assertFalse(cs.isKEGGSpeciesConsistent(['GO:0035556'], libsbml.Species))
 
-
-
-
-
+  def testIsKEGGProcessConsistent(self):
+    # normally expected case
+    self.assertFalse(cs.isKEGGProcessConsistent(['C00062'], libsbml.Species))
+    # compartment should not be a KEGG species term
+    self.assertTrue(cs.isKEGGProcessConsistent(['ko00061'], libsbml.Model))
+    # GO term is not a KEGG term, should be rejected
+    self.assertTrue(cs.isKEGGProcessConsistent(['R00259'], libsbml.Reaction))
 
 
 
