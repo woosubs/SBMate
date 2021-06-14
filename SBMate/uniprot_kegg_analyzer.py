9b21bfb35bdb5c7480858ff03707db9ba731d578
Woosub Shin <woosubs@umich.edu>
2021-06-15 01:10:17 +0900

    fixed bug (sbmate vs. setup.py)

--- SBMate/uniprot_kegg_analyzer.py
+++ SBMate/uniprot_kegg_analyzer.py
@@ -39,11 +39,49 @@ class NonDAGAnalyzer(object):
     self.term_id = term_id
     self.ontology = ontology
     self.object_type = object_type
+    self.consistency = None
 
   def getOneTermConsistency(self, one_term):
     """
     Get consistency of one term,
-    by connecting to the url
+    by connecting to the url.
     :param str one_term:
-    :return float:
+    :return bool:
     """
+    r = requests.get(ONT_TO_URL[self.ontology]+one_term)
+    # for kegg, needs to check whether the text is in the page
+    if KEGG_ERROR_MESSAGE in r.text:
+      return False
+    else:
+      return r.ok
+
+  def getConsistency(self, inp_term):
+  	"""
+  	Using getOneTermConsistency(),
+  	check if the terms are consistent.
+  	"""
+    # first, check if the input term is in correct format
+    if isinstance(inp_term, str):
+      inp_list = [inp_term]
+    elif isinstance(inp_term, list):
+      # if it is a list, check whether all terms are string
+      if sum([isinstance(t, str) for t in inp_term])==len(inp_term):
+        inp_list = inp_term
+      else:
+      	return False
+    else:
+      return False
+    # next, check consistency
+
+
+
+
+
+
+
+
+
+
+
+
+
