767f18201592721ff4e2566e7d7f1e8e3e3b0fcf
woosubs <woosubs@umich.edu>
2021-06-15 01:28:21 +0900

    Rearranged sbmate.py and setup.py. Started none DAG analyzer (#15)
    * dag_analyzer started, created setup.py
    
    * started uniprot_kegg_analyzer, setup.py moved
    
    * fixed bug (sbmate vs. setup.py)

--- SBMate/uniprot_kegg_analyzer.py
+++ SBMate/uniprot_kegg_analyzer.py
@@ -17,6 +17,13 @@ import requests
 from SBMate import constants as cn
 
 
+ONT_TO_URL: {"uniprot":"https://www.genome.jp/entry/",
+           "kegg_process":"https://www.genome.jp/entry/",
+           "kegg_species":"https://www.genome.jp/entry/",
+           }
+KEGG_ERROR_MESSAGE: "No such data was found"
+
+
 class NonDAGAnalyzer(object):
   """
   Analyzer for UNIPROT and KEGG.
@@ -33,4 +40,40 @@ class NonDAGAnalyzer(object):
     self.term_id = term_id
     self.ontology = ontology
     self.object_type = object_type
-    
+    self.consistency = None
+
+  def getOneTermConsistency(self, one_term):
+    """
+    Get consistency of one term,
+    by connecting to the url.
+    :param str one_term:
+    :return bool:
+    """
+    r = requests.get(ONT_TO_URL[self.ontology]+one_term)
+    # for kegg, needs to check whether the text is in the page
+    if KEGG_ERROR_MESSAGE in r.text:
+      return False
+    else:
+      return r.ok
+
+  def getConsistency(self, inp_term):
+  	"""
+  	Using getOneTermConsistency(),
+  	check if the terms are consistent.
+  	"""
+    # first, check if the input term is in correct format
+    if isinstance(inp_term, str):
+      inp_list = [inp_term]
+    elif isinstance(inp_term, list):
+      # if it is a list, check whether all terms are string
+      if sum([isinstance(t, str) for t in inp_term])==len(inp_term):
+        inp_list = inp_term
+      else:
+      	return False
+    else:
+      return False
+    # next, check consistency
+
+
+
+
